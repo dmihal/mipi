b9a170048f41a535c35030e11649539030a5b696
David Mihal <eastsideski@gmail.com>
2012-04-19 20:38:43 -0400

    first

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>templates</title>
+  <meta name="description" content="" />
+  <meta name="author" content="David Mihal" />
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+</head>
+
+<body>
+  <div>
+    <header>
+      <h1>Login</h1>
+    </header>
+
+    <div>
+      <form action="/mipi/" method="post">
+      	<label>Username: <input name="user" /></label><br />
+      	<label>Password: <input type="password" name="password" /></label><br />
+      	<button type="submit">Login</button>
+      </form>
+    </div>
+
+    <footer>
+     <p>&copy; Copyright  by David Mihal</p>
+    </footer>
+  </div>
+</body>
+</html>
