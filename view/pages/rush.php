5d024dff1913b0a7ad3fb118f1fc12cb55380e47
David Mihal <eastsideski@gmail.com>
2012-04-25 14:10:15 -0400

    Show rushees from DB

--- view/pages/rush.php
+++ view/pages/rush.php
@@ -1,17 +1,30 @@
 <?php
-$page = new Page("Rush");
-
-$box = new Box("rush","Rush");
-$peoplelist = new BCPeopleList();
-$peoplelist->defaultState = 'thumbnail';
-$peoplelist->setColumns("Email","Social","Cell Phone","Pi Number","YOG","Birthday");
-$peoplelist->setThumbColumns("Pi Number");
-
-$peoplelist->addPerson("Warren", "Smalle", "user/12345", "",
-			"abeleveau@wpi.edu",'<a href="http://www.facebook.com/andrew.beliveau">Facebook</a>',"123456789","&#960;1643","2012","June 8, 1989");
-
-$box->setContent($peoplelist);
-$page->addBox($box,'tripple');
+$user = getUser();
+switch (@$_GET[1]) {
+	case 'person':
+		$page = Page::getPage('rush/person');
+		break;
+	default:
+		$page = new Page("Rush");
+		
+		$box = new Box("rush","Rush");
+		$peoplelist = new BCPeopleList();
+		$peoplelist->defaultState = 'thumbnail';
+		$peoplelist->setColumns("Email","Social","Cell Phone","Year");
+		$peoplelist->setThumbColumns("Year");
+		
+		foreach (Rushee::getRusheesFromQuery("SELECT * FROM rushees ") as $rushee) {
+			/* @var $rushee Rushee */
+			$peoplelist->addPerson($rushee->first, $rushee->last, "rush/person/$rushee->id", $rushee->getPhotoPath(),$rushee->email,'',$rushee->phone,$rushee->getYearName());
+		}
+		
+		//$peoplelist->addPerson("Warren", "Smalle", "user/12345", "",
+		//			"abeleveau@wpi.edu",'<a href="http://www.facebook.com/andrew.beliveau">Facebook</a>',"123456789","&#960;1643","2012","June 8, 1989");
+		
+		$box->setContent($peoplelist);
+		$page->addBox($box,'tripple');
+		break;
+}
 $page->section = "rush";
 return $page;
 ?>
