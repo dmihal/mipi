9431179e5710cd0a95d3d3e69c90dc1436b215cd
David Mihal <eastsideski@gmail.com>
2012-05-26 11:42:58 -0400

    Added columns

--- view/pages/events/listoflists.php
+++ view/pages/events/listoflists.php
@@ -1,7 +1,11 @@
 <?php
 $page = new Page("Guest Lists");
 
+$open = new Box("open","Open Lists");
+$page->addBox($open,'double');
 
+$closed = new Box("closed","Closed Lists");
+$page->addBox($closed,'right');
 
 return $page;
 ?>
