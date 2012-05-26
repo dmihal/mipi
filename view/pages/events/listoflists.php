2b42913cd4fd2c2b3eb92d005592156447b07737
David Mihal <eastsideski@gmail.com>
2012-05-26 12:11:07 -0400

    display open and closed lists

--- view/pages/events/listoflists.php
+++ view/pages/events/listoflists.php
@@ -2,9 +2,33 @@
 $page = new Page("Guest Lists");
 
 $open = new Box("open","Open Lists");
+$guestList = new BCList();
+try{
+	$events = Event::getEventsFromQuery(Event::QueryOpenLists);
+	foreach ($events as $key => $event) {
+		/* @var $event Event */
+		$guestList->addElement($event->name, "", "","events/list/".$event->id);
+	}
+	unset($events);
+}catch(Exception $e){
+	$guestList = new BCStatic("No open guest lists");
+}
+$open->setContent($guestList);
 $page->addBox($open,'double');
 
 $closed = new Box("closed","Closed Lists");
+$guestList = new BCList();
+try{
+	$events = Event::getEventsFromQuery(Event::QueryClosedLists);
+	foreach ($events as $key => $event) {
+		/* @var $event Event */
+		$guestList->addElement($event->name, "", "","events/list/".$event->id);
+	}
+	unset($events);
+}catch(Exception $e){
+	$guestList = new BCStatic("No open guest lists");
+}
+$closed->setContent($guestList);
 $page->addBox($closed,'right');
 
 return $page;
