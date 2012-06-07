d9143e3bccb943fda9ed52369862a42408771c0b
David Mihal <eastsideski@gmail.com>
2012-06-06 23:59:25 -0400

    Show list of messages

--- view/pages/messages.php
+++ view/pages/messages.php
@@ -1,12 +1,25 @@
 <?php
-$page = new Page("Messages");
+switch (@$_GET[1]) {
+	case 'compose':
+		$page = Page::getPage('messages/compose');
+		break;
+	default:
+		$page = new Page("Messages");
+        
+        $inbox = new Box("inbox","Inbox");
+        
+        $msgList = new BCList();
+        $messages = Message::getUserMessages();
+        foreach ($messages as $key => $message) {
+        	/* @var $message Message */
+        	$msgList->addElement($message->subject, $message->getSender()->getName(), $message->getPreview(),'#','/user/'.$message->getSender()->id,$message->date->format('m/d/Y'));
+        }
+        $inbox->setContent($msgList);
+        $page->addBox($inbox,'tripple');
+        
+        $page->section = "message";
+		break;
+}
 
-$inbox = new Box("inbox","Inbox");
-$msgList = new BCList();
-$msgList->addElement("New Message System", "David Mihal", "This is the first message","#","#","3/7/2012");
-$inbox->setContent($msgList);
-$page->addBox($inbox,'tripple');
-
-$page->section = "message";
 return $page;
 ?>
