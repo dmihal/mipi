0cd870fb26e30d676d0bd8e70cefaf63c5b3fb5a
David Mihal <eastsideski@gmail.com>
2012-06-09 13:31:42 -0400

    Working message form

--- view/pages/messages/compose.php
+++ view/pages/messages/compose.php
@@ -12,10 +12,14 @@ $page->js = ob_get_clean();
 $box = new Box('composer','Compose Message');
 ob_start();
 ?>
-<label>To: <input id="tofield" name="to" /></label><input name="toid" id="toid" type="hidden" /><br />
-<label>Confidential: <input type="checkbox" name="confidential" /></label><br />
-<textarea name="message" style="width: 99%;height: 400px;"></textarea><br />
-<button>Send</button>
+<form method="post" action="/mipi/messages/send">
+    <label>To: <input id="tofield" name="to" /></label><br />
+    <label>Subject: <input name="subject" /></label><br />
+    <label>Confidential: <input type="checkbox" name="confidential" /></label><br />
+    <textarea name="message" style="width: 99%;height: 400px;"></textarea><br />
+    <button>Send</button>
+</form>
+
 <?php
 $box->setContent(new BCStatic(ob_get_clean()));
 $page->addBox($box,'tripple');
