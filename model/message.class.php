d6e1990567f5edef462b514fc32f0893f18c4b7a
David Mihal <eastsideski@gmail.com>
2012-06-09 13:30:43 -0400

    newMessage function

--- model/message.class.php
+++ model/message.class.php
@@ -30,6 +30,28 @@ class Message {
         return Member::getMember($this->sender);
     }
     /**
+     * undocumented function
+     *
+     * @return void
+     * @author  
+     */
+    static function sendMessage($to,$message,$subject="No Subject",$confidential=false) {
+        $querybuilder = new SqlQueryBuilder('insert');
+        $querybuilder->setTable('messages');
+        $querybuilder->addPair('sender', getUser()->id);
+        $querybuilder->addPair('message', $message);
+        $querybuilder->addPair('subject', $subject);
+        $msgID = Query::insert($querybuilder->buildQuery());
+        
+        foreach ($to as $personID) {
+            $querybuilder = new SqlQueryBuilder('insert');
+            $querybuilder->setTable('message_status');
+            $querybuilder->addPair('message', $msgID);
+            $querybuilder->addPair('user', $personID);
+            Query::insert($querybuilder->buildQuery());
+        }
+    }
+    /**
      * Get the number of unread messages for a user, defaults to current
      *
      * @return void
@@ -40,7 +62,7 @@ class Message {
         $query = "SELECT COUNT(*) AS num FROM  `message_status` 
                 WHERE  `user` = $user->id AND  `read` =  'FALSE'";
         $query = new Query($query);
-        return $query->numRows;
+        return $query->getField('num');
     }
     /**
      * Get a preview of the message
