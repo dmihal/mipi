2aa7c6962fc9591165a6efd6f4acad89975bf728
David Mihal <eastsideski@gmail.com>
2012-07-22 12:30:33 -0400

    created Hyperlink class

---
+++ view/hyperlink.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class representing the attributes of a hyperlink
+ *
+ * @package default
+ * @author  
+ */
+class Hyperlink {
+    public $title,$url,$class;
+    
+    public function __construct($title,$url='#',$class='')
+    {
+        $this->title = $title;
+        $this->class = $class;
+        $this->url   = $url;
+    }
+    
+    /**
+     * Return HTML <a> tag
+     *
+     * @return string
+     * @author  
+     */
+    public function getHTML() {
+        return "<a href=\"$this->url\" class=\"$this->class\">$this->title</a>";
+    }
+    public function __toString() {
+        return $this->getHTML();
+    }
+    /**
+     * Add class to string
+     *
+     * @return Hyperlink
+     * @author  
+     */
+    function addClass($class) {
+        $this->class .= " $class";
+        return $this;
+    }
+} // END
+?>
