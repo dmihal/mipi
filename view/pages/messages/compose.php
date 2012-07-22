d3d0fe044cbf5c8b132ed166f6dbc7b89693fe1b
David Mihal <eastsideski@gmail.com>
2012-07-22 12:34:05 -0400

    Added support for replies and to:

--- view/pages/messages/compose.php
+++ view/pages/messages/compose.php
@@ -1,22 +1,52 @@
 <?php
 $page = new Page("Compose Message");
 
+$reply;
+try {
+    $reply = Message::getMessage(@$_GET['r']);
+} catch (Exception $e) {
+    $reply = null;
+}
+$to = (isset($_GET['to'])) ? Member::getMember($_GET['to']) : null;
+
 ob_start();
+$tolkenSetings = "";
+if ($reply){
+    echo "var to = ";
+    $recipients = $reply->getRecipients(true);
+    $jsonarray = array();
+    for ($i=0; $i < count($recipients); $i++) { 
+        if ($recipients[$i]==getUser()) {
+            unset($recipients[$i]);
+        } else {
+            $jsonarray[] = array("id"=>$recipients[$i]->id, "name"=>$recipients[$i]->getName());
+        }
+        
+    }
+    echo json_encode($jsonarray).";";
+    $tolkenSetings = ",prePopulate:to,disabled:true";
+} elseif($to) {
+    echo "var to = [{id:$to->id,name:\"".$to->getName()."\"}]";
+    $tolkenSetings = ",prePopulate:to";
+}
 ?>
+
 $(function(){
-    $("#tofield").tokenInput("/mipi/ajax/membertoken",{theme:'facebook'});
+    $("#tofield").tokenInput("/mipi/ajax/membertoken",{theme:'facebook'<?php echo $tolkenSetings; ?>});
 });
 <?php
 $page->js = ob_get_clean();
 
 $box = new Box('composer','Compose Message');
 ob_start();
+
+$rSubj = $reply ? 'disabled="true" value="'.$reply->subject.'" ' : "";
 ?>
 <form method="post" action="/mipi/messages/send">
     <label>To: <input id="tofield" name="to" /></label><br />
-    <label>Subject: <input name="subject" /></label><br />
+    <label>Subject: <input name="subject" <?php echo $rSubj ?>/></label><br />
     <label>Confidential: <input type="checkbox" name="confidential" /></label><br />
-    <textarea name="message" style="width: 99%;height: 400px;"></textarea><br />
+    <textarea name="message" style="width: 600px;height: 200px;"></textarea><br />
     <button>Send</button>
 </form>
 
