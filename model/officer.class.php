bbfb5dda3eee54c2d6638c11d766e6735860c7c3
David Mihal <eastsideski@gmail.com>
2012-07-31 23:19:55 -0400

    Officer class reengineered

--- model/officer.class.php
+++ model/officer.class.php
@@ -5,24 +5,22 @@
  * @package default
  * @author  
  */
-class Officer {
+class Officer extends Overloadable{
 	
 	public $id,$name,$title,$subtitle,$memberID;
+    
+    private static $library;
 	
-	public function __construct($name)
+	public function __construct(array $data)
 	{
-		$this->name = $name;
-		
-		$q = new Query("SELECT * FROM officers WHERE `name`='$name'");
-		if($q->numRows!=1)
-		{
-			throw new Exception("Officer query failure", 1);
-		} else {
-			$this->id		= $q->getField("ID");
-			$this->memberID	= $q->getField("member");
-			$this->title	= $q->getField("title");
-			$this->subtitle	= $q->getField("subtitle");
-		}
+		$this->name       = $data['name'];
+		$this->id         = $data["ID"];
+		$this->memberID	  = $data["member"];
+		$this->title      = $data["title"];
+		$this->subtitle	  = $data["subtitle"];
+        $this->hiddenData(json_decode($data['data'],true));
+        
+        self::$library->add($this->id,$this);
 	}
 	/**
 	 * Get the Member object of the officer
@@ -43,6 +41,75 @@ class Officer {
 	{
 		return Announcement::getAnnouncementsFromQuery("SELECT * FROM `announcements` WHERE `officer`=$this->id ORDER BY `date`");
 	}
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author  
+     */
+    function getLink() {
+        return new Hyperlink($this->name,"/officer/$this->name");
+    }
+    /**
+     * Get officer by ID
+     *
+     * @return Officer
+     * @author  
+     */
+    static function getOfficer($id) {
+        $officers = self::getOfficersByQuery(sprintf("SELECT * FROM officers WHERE `ID`=%d",$id));
+        return $officers[0];
+    }
+    /**
+     * undocumented function
+     *
+     * @return Officer
+     * @author  
+     */
+    static function getOfficerByName($name) {
+        $officers = self::getOfficersByQuery("SELECT * FROM officers WHERE `name`='$name'");
+        return $officers[0];
+    }
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author  
+     */
+    static function getOfficersByUser($user) {
+        $id = ($user instanceof Member) ? $user->id : $user;
+        return self::getOfficersByQuery("SELECT * FROM officers WHERE `member`=$id;");
+    }
+    /**
+     * undocumented function
+     *
+     * @return array
+     * @author  
+     */
+    static function getOfficersByQuery($query) {
+        if(!isset(self::$library)){
+            self::$library = new Library();
+        }
+        
+        $query = new Query($query);
+        if ($query->numRows >= 1) {
+            $array = array();
+            while($row = $query->nextRow())
+            {
+                $officer;
+                if (self::$library->exists($row['ID'])){
+                    $officer = self::$library->get($row['ID']);
+                } else {
+                    $officer = new Officer($row);
+                }
+                
+                $array[] = $officer;
+            }
+            return $array;
+        } else {
+            throw new Exception();
+        }
+    }
 	static function getOfficerLists($elected=true)
 	{
 		$elected = $elected ? 'true' : 'false';
@@ -54,5 +121,6 @@ class Officer {
 		}
 		return $officers;
 	}
+    
 } // END
 ?>
