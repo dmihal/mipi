22889526229b33aaffe2a7cd9fca52a0cb9ec99d
David Mihal <eastsideski@gmail.com>
2012-07-31 23:22:05 -0400

    Changed to be an subclass of Hyperlink including getHTML method

--- view/navelement.class.php
+++ view/navelement.class.php
@@ -2,12 +2,24 @@
 /**
  * 
  */
-class NavElement {
-	
-	public $link, $title, $class;
-	
-	function __construct($title, $link="#", $class="") {
-		list($this->link, $this->title, $this->class) = array($link, $title, $class);
+class NavElement extends Hyperlink {
+    
+    const trans = "data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
+    
+	public function getHTML()
+	{
+		ob_start();
+        ?>
+<li>
+    <a href="<?php echo $this->url ?>">
+        <div class="navIcon">
+            <img src="<?php echo self::trans ?>" alt="<?php echo $this->title ?>" id="icon<?php echo ucfirst($this->class) ?>" />
+        </div>
+        <?php echo $this->title ?>
+    </a>
+</li>
+<?php
+        return ob_get_clean();
 	}
 }
 
