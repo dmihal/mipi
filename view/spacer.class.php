b8528f0a7babdff309ea7cef1e5a43eec86060d3
David Mihal <eastsideski@gmail.com>
2012-07-31 23:23:37 -0400

    first commit

---
+++ view/spacer.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author  
+ */
+class Spacer implements HTMLElement {
+    public function getHTML(){
+        echo "<li style=\"height:6px;\">&nbsp</li>";
+    }
+} // END
+?>
