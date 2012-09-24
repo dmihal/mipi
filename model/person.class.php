fe7d2f200ca96a8a9918ad8f5189915dba96bcda
David Mihal <eastsideski@gmail.com>
2012-09-24 11:26:16 -0400

    Added sex (giggity)

--- model/person.class.php
+++ model/person.class.php
@@ -7,19 +7,24 @@
  */
 class Person extends Overloadable {
 	public $first, $last, $yog, $id;
-	public $phone, $email, $dob;
+	public $phone, $email, $dob, $sex;
 	
 	public $type;
 	const RUSHEE = 1;
 	const AM = 2;
 	const BROTHER = 3;
 	const ALUMNI = 4;
+    
+    const MALE = 6;
+    const FEMALE = 7;
 	
-	public function __construct($first=NULL,$last=NULL,$id=NULL)
+	public function __construct($first=NULL,$last=NULL,$id=NULL,$sex=NULL)
 	{
 		$this->first	= $first;
 		$this->last		= $last;
 		$this->id		= $id;
+        
+        $this->setSex($sex);
 	}
 	
 	/**
@@ -31,5 +36,16 @@ class Person extends Overloadable {
 	function getName($lastfirst=false) {
 		return $lastfirst ? $this->last . ', ' . $this->first : $this->first . ' ' . $this->last;
 	}
+    function getSex(){
+        return ($this->sex==self::FEMALE) ? "Girl" : "Guy";
+    }
+    public function setSex($sex)
+    {
+        if ($sex == self::FEMALE or $sex == "Girl") {
+            $this->sex = self::FEMALE;
+        } elseif($sex == self::MALE or $sex == "Guy"){
+            $this->sex = self::MALE;
+        }
+    }
 } // END
 ?>
