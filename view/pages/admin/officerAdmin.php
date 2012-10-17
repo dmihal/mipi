d921b47781c8bcf591a956419c5628e976f28cab
David Mihal <eastsideski@gmail.com>
2012-10-16 23:11:06 -0400

    Try/catch for announcements

--- view/pages/admin/officerAdmin.php
+++ view/pages/admin/officerAdmin.php
@@ -4,18 +4,22 @@ $officer = Officer::getOfficerByName(@$_GET['officer']);
 $page = new AdminPage($officer->name,getUser(),array($officer->name));
 
 $announcements = new Box("announcements","Announcements");
-$announceList = new BCList();
-ob_start();
-?>
-<a href="/admin/announcement/new:<?php echo $officer->name ?>">New Announcement</a>
-<?php
-$announceList->header = ob_get_clean();
-foreach ($officer->getAnnouncements() as $announcement) {
-    /* @var $announcement Announcement */
-    $link = new Hyperlink($announcement->title,"/admin/announcement/".$announcement->id,"");
-  	$announceList->addElement($link, "",$announcement->body,$announcement->date->format('m/d/Y'));
+try {
+    $announceList = new BCList();
+    ob_start();
+    ?>
+    <a href="/admin/announcement/new:<?php echo $officer->name ?>">New Announcement</a>
+    <?php
+    $announceList->header = ob_get_clean();
+    foreach ($officer->getAnnouncements() as $announcement) {
+        /* @var $announcement Announcement */
+        $link = new Hyperlink($announcement->title,"/admin/announcement/".$announcement->id,"");
+      	$announceList->addElement($link, "",$announcement->body,$announcement->date->format('m/d/Y'));
+    }
+    $announcements->setContent($announceList);
+} catch (Exception $e) {
+    $announcements->setContent(new BCStatic('<a href="/admin/announcement/new:'.$officer->name .'">New Announcement</a><br /><br />No announcements'));
 }
-$announcements->setContent($announceList);
 $page->addBox($announcements);
 
 return $page;
