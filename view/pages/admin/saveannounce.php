8c683da97d026a493a45cbe3fb96323b99468519
David Mihal <eastsideski@gmail.com>
2013-04-25 14:28:04 -0400

    Return to the officer after saving anouncement

--- view/pages/admin/saveannounce.php
+++ view/pages/admin/saveannounce.php
@@ -1,5 +1,5 @@
 <?php
 $officer = Officer::getOfficer($_POST['officer']);
 $announcement = Announcement::newAnnouncement($_POST['officer'], getUser()->id, $_POST['title'], $_POST['body']);
-header 
+header("/officer/".$_POST['officer']);
 ?>
