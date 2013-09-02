24180406856fa097bec71bfbc41a67fdb1f1ac4f
David Mihal <eastsideski@gmail.com>
2013-09-02 12:52:15 -0400

    Use bootstrap login

--- login.php
+++ login.php
@@ -1,40 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8" />
+  <head>
+    <meta charset="utf-8">
+    <title>Sign in &middot; MyPiZeta</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!-- Le styles -->
+    <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
 
-  <title>templates</title>
-  <meta name="description" content="" />
-  <meta name="author" content="David Mihal" />
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
 
-  <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+    </style>
+    <link href="/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+  </head>
 
-  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-  <link rel="shortcut icon" href="/favicon.ico" />
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-</head>
+  <body>
 
-<body>
-  <div>
-    <header>
-      <h1>Login</h1>
-    </header>
+    <div class="container">
 
-    <div>
-      <form action="/" method="post">
-      	<label>Username: <input name="user" /></label><br />
-      	<label>Password: <input type="password" name="password" /></label><br />
-      	<button type="submit">Login</button>
+      <form class="form-signin" action="/" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" name="user" class="input-block-level" placeholder="Email address">
+        <input type="password" password="password" class="input-block-level" placeholder="Password">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
       </form>
-    </div>
 
-    <footer>
-     <p>&copy; Copyright  by David Mihal</p>
-    </footer>
-  </div>
-</body>
+    </div> <!-- /container -->
+  </body>
 </html>
